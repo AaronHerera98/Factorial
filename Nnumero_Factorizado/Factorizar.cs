e18a99f0e8523b5e1a6ac16c4bee7ed7fc53ed20
Aaron Herrera <aaron.ssss62@gmail.com>
2019-02-11 11:28:47 -0600

    Se creo nuevo repro

---
+++ Nnumero_Factorizado/Factorizar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nnumero_Factorizado
+{
+    class Factorizar
+    {
+        public int num { get; set; }
+        public int numero { get; set; }
+
+        public Factorizar()
+        {
+            num = 0;
+            numero = 0;
+        }
+        public void factor()
+        {
+
+            for (int i = 0; i >= num; i++)
+            {
+                i = i + 1;
+
+                num = num * i;
+
+                numero = numero + num;
+            }
+            
+        }
+    }
+}
---
+++ Nnumero_Factorizado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nnumero_Factorizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el numero a factorizar");
+
+            Factorizar obj = new Factorizar();
+
+            obj.num = Convert.ToInt32(Console.ReadLine());
+
+            obj.factor();
+
+            Console.WriteLine("El numero es: "+ obj.numero);
+
+            Console.ReadKey();
+
+        }
+    }
+}
